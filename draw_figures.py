9e469290637b12e795c974a38185d4dc2f65e061
Jean-David Fiquet <jdfiquet2@gmail.com>
2015-11-24 18:54:13 +0100

    Add the mean computation for several sets of clients

--- draw_figures.py
+++ draw_figures.py
@@ -2,11 +2,37 @@ from numpy.random import RandomState
 from taciturn_wookie import *
 
 prng = RandomState(131252476)
-w = TaciturnWorker(second_law_name = prng.exponential,
-    first_law_parameter = 30,
-    second_law_parameter = 10, 
-    rng = prng, 
-    poisson_parameter = 50, 
-    clients_number = 100000)
+poisson_parameters = [1,10,25,50]
+clients_numbers = [10, 100, 1000, 5000, 10000, 100000, 250000]
 
-print w.proceed()
+exponential_results = []
+exponential_crude_values = []
+exponential_parameter = 10
+uniform_parameter = 30
+
+for poisson_parameter in poisson_parameters:
+    for clients_number in clients_numbers:
+
+        w = TaciturnWorker(second_law_name = prng.exponential,
+            first_law_parameter = uniform_parameter,
+            second_law_parameter = exponential_parameter, 
+            rng = prng, 
+            poisson_parameter = poisson_parameter, 
+            clients_number = clients_number)
+        values = w.proceed()
+        response_time_values = map(lambda x: x[2], values)
+        exponential_crude_values.append({
+            'second_law': 'exponential',
+            'clients_number': clients_number,
+            'poisson_parameter': poisson_parameter,
+            'values': values})
+        
+        mean = reduce(lambda x, y: x + y, response_time_values)/clients_number
+        exponential_results.append({
+            'second_law': 'exponential',
+            'clients_number': clients_number,
+            'poisson_parameter': poisson_parameter,
+            'mean': mean
+        })
+        
+print exponential_results
